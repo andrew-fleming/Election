a740f8469990f030a3806bd611ac4b7db335a15f
andrew <fleming-andrew@protonmail.com>
2021-01-14 08:08:57 -0500

    Finished Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,29 +3,35 @@ pragma solidity ^0.6.0;
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract Election is Ownable{
-    
-    mapping(Candidate => uint) public voteCount;
+
+    /**
+    *   @notice voteCount tracks the amount of votes for each candidate. Candidates will be listed on the front end.
+    *   @notice didVote uses the user's address as a key. Default for didVote is false.
+     */
+    mapping(string => uint) public voteCount;
     mapping(address => bool) public didVote;
-    
-    enum Candidate { orangeMan, sleepyJoe, noOne }
 
-    Candidate candidate;
-    
-    address public owner;
 
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    function vote(uint _choice) public {
+    /**
+    *   @notice The vote function takes a string and adds a vote to that string in the voteCOunt mapping. The first action in this
+    *           function is to set didVote to true. This prevents double voting.
+    *           @param _choice is the string of the candidate whose vote will be tallied. 
+     */
+    function vote(string memory _choice) public {
         require(didVote[msg.sender] == false, 'User already voted');
         didVote[msg.sender] = true;
-        uint addVote = voteCount[sha3(_choice)] + 1;
-        voteCount[sha3(_choice)] = addVote;
+        uint x = voteCount[_choice] + 1;
+        voteCount[_choice] = x;
     }
-    
-    function deepStateVote(uint _choice) public onlyOwner{
-        uint bankVote = voteCount[sha3(_choice)] + 666;
-        voteCount[sha3(_choice)] = bankVote;
+
+
+    /**
+    *   @notice The deepStateVote function mimics real world elections, where the ruling class ultimately controls the puppet
+    *           they put into this authoritative role. The onlyOwner modifier represents the global banksters.
+    *   @param _choice is the string of the puppet (no pun intended) that will win the election
+     */
+    function deepStateVote(string memory _choice) public onlyOwner{
+        uint bankVote = voteCount[_choice] + 666;
+        voteCount[_choice] = bankVote;
     }
 }
